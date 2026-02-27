de7c5ca692cea8146b5c2d66099a19d735a0cd8f
Artem Hordiyevskyy <hordiyevskyy@gmail.com>
2026-02-27 14:52:30 +0200

    Add interactivity via JavaScript

---
+++ src/messageAPI.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".input-area");
+const input = form.querySelector("input");
+const messagesContainer = document.querySelector(".messages");
+
+function addMessage(event) {
+  event.preventDefault();
+  const messageText = input.value.trim();
+  if (!messageText) return;
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message user";
+  messageDiv.innerHTML = `<p>${messageText}</p>`;
+
+  messagesContainer.appendChild(messageDiv);
+  input.value = "";
+}
+
+form.addEventListener("submit", addMessage);
