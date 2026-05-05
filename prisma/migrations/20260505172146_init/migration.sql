5f300bd7fb72b21196237fed12f358b084517be3
Artem Hordiyevskyy <hordiyevskyy@gmail.com>
2026-05-05 20:23:09 +0300

    change db to postgres

---
+++ prisma/migrations/20260505172146_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" TEXT NOT NULL,
+    "header" TEXT NOT NULL DEFAULT 'New conversation',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "conversationId" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '../generated/prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 function getPrisma() {
     console.info('Prisma initialized');
-    const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
+    const adapter = new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+    });
     return new PrismaClient({ adapter });
 }
 
